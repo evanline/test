1676dd9c5f7de20c4ceba0a88a26367e096eaee5
evanline <ietsenniemand@gmail.com>
2016-02-21 19:55:24 +0100

    update

--- cijferinvoeren.php
+++ cijferinvoeren.php
@@ -45,7 +45,7 @@ if(isset($_POST['submit'])){
             $error[3] = "Vul een leerlingnummer in:";
         }
         if (empty($error)){
-             header("location: congrats.php");
+             header("location: cijferinvoeren.php");
         }
         //sql voor plaatsen
         //$sql = "INSERT INTO vakken(vakcode, cijfer, EC, leerlingnummer) VALUES ('$code', '$cijfer', '$EC', '$leerlingnummer')";
@@ -62,7 +62,7 @@ if(isset($_POST['submit'])){
     </head>
     <body>
         <div class= "toevoeging">
-            <form method="post" action="cijferinvoeren.php">
+            <form method="post" action="vaktoegevoegd.php">
         <h3 class="titel">cijfer invoeren</h3>
 
             <p class='gegevens'>Code:</p>
